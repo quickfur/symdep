90f64b342e66743d3638e2621e20aa430ac9ccf5
H. S. Teoh <hsteoh@quickfur.ath.cx>
2013-09-13 12:12:17 -0700

    Finish implementing parseSymDeps.
    With unittest. That caught about, oh, 3 bugs instantly? :)

--- symdep.d
+++ symdep.d
@@ -6,9 +6,17 @@
 import std.conv;
 import std.process;
 import std.range;
+import std.regex;
 import std.stdio;
+import std.string : splitLines;
+import std.typecons : tuple;
+
+version(unittest)
+    import std.algorithm;
+
 
 immutable objdump = "/usr/bin/objdump";
+
 
 /**
  * Parses objdump output to extract symbol dependencies.
@@ -20,9 +28,7 @@ immutable objdump = "/usr/bin/objdump";
 auto parseSymDeps(R)(R lines)
     if (isInputRange!R && is(ElementType!R : const(char)[]))
 {
-    import std.typecons : Tuple, tuple;
-
-    struct Result
+    static struct Result
     {
         private R src;
         private string curSym;
@@ -35,19 +41,26 @@ auto parseSymDeps(R)(R lines)
         {
             src = _src;
             reSymDef = regex(`^[0-9a-f]+\s+<([^>]+)>:\s*$`);
-            reSymDep = regex(`<([^>+]+)(?:+(?:0x)?[0-9a-f]+)>`);
+            reSymDep = regex(`<([^>+]+)(?:\+(?:0x)?[0-9a-f]+)?>`);
             getNext();
         }
 
         // Range API
         @property bool empty() { return src.empty; }
         @property auto front() { return tuple(curSym, curDep); }
-        void popFront() { getNext(); }
+        void popFront()
+        {
+            src.popFront(); // skip over current match
+            getNext();
+        }
 
         private void getNext()
         {
             while (!src.empty)
             {
+                // See if we're on a new symbol definition (this must be done
+                // first, since reSymDep may otherwise wrongly match the name
+                // of the new symbol instead of a real dependency).
                 auto m = src.front.match(reSymDef);
                 if (m)
                 {
@@ -55,9 +68,11 @@ auto parseSymDeps(R)(R lines)
                 }
                 else
                 {
-                    auto m = src.front.match(reSymDep);
+                    // Not a symbol definition; see if there's a dependency.
+                    m = src.front.match(reSymDep);
                     if (m)
                     {
+                        // Found dependency: yield.
                         curDep = to!string(m.captures[1]);
                         break;
                     }
@@ -66,7 +81,28 @@ auto parseSymDeps(R)(R lines)
             }
         }
     }
+    static assert(isInputRange!Result);
+
     return Result(lines);
+}
+
+unittest
+{
+    string[] sampleInput = splitLines(q"END
+000000000049b228 <_D3std7process7__arrayZ>:
+  49b228:	55                   	push   %rbp
+  49b229:	48 8b ec             	mov    %rsp,%rbp
+  49b22c:	48 83 ec 10          	sub    $0x10,%rsp
+  49b230:	48 89 fe             	mov    %rdi,%rsi
+  49b233:	48 bf c0 5d 6e 00 00 	movabs $0x6e5dc0,%rdi
+  49b23a:	00 00 00 
+  49b23d:	e8 7e a6 ff ff       	callq  4958c0 <_d_array_bounds>
+  49b242:	66 0f 1f 44 00 00    	nopw   0x0(%rax,%rax,1)
+END");
+    auto deps = parseSymDeps(sampleInput);
+    assert(deps.equal([
+        tuple("_D3std7process7__arrayZ", "_d_array_bounds")
+    ]));
 }
 
 void buildSymGraph(R)(R lines)
